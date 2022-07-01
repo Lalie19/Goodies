dec90534a92257f6ba3756fef23fab5adebe65db
Eulalie <petitlalie06@gmail.com>
2022-07-01 14:38:37 +0200

    image home

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,17 +27,7 @@ class RegistrationFormType extends AbstractType
                 ],
 
             ])
-            // ->add('agreeTerms', CheckboxType::class, [
-            //     'mapped' => false,
-            //     'constraints' => [
-            //         new IsTrue([
-            //             'message' => 'You should agree to our terms.',
-            //         ]),
-            //     ],
-            // ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'label' => 'votre mot de passe :',
                 'mapped' => false,
                 'attr' => [
@@ -51,7 +41,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} characteres',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -30,6 +30,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
+        
         $email = $request->request->get('email', '');
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
