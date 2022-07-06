97ed482e9798e148ef93425e7d8dc45689d11cd9
Eulalie <petitlalie06@gmail.com>
2022-07-06 23:03:56 +0200

    com

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -19,15 +19,20 @@ class StripeController extends AbstractController
     #[Route('/{commande}/stripe', name: 'stripe_checkout')]
     public function check(Commande $commande, AchatsRepository $achatsRepository, Request $request, EntityManagerInterface $entityManagerInterface): Response
     {
+        // récupérer la clé du compte stripe
         $privateKey = "sk_test_51KbPeEGLGNzn2cmOeQZIMSYG90TCdqnFg4cNSk7BIi9RL8ovAmYVZu6xZUSnZrMMkgojOvkuH1MQBMPykYSzc8xH00XZnPta92";
+        // appelé stripe
         Stripe::setApikey($privateKey);
 
+        // // récuper les comamndes pour la facture 
         $achatCriteriav = [
             "commande" => $commande,
         ];
         $achatses = $achatsRepository->findBy($achatCriteriav);
+        
         $lineItems = [];
         foreach ($achatses as $achat) {
+            // récupere les données 
             $item = [
                 "price_data" => [
                     "currency" => "eur",
@@ -38,20 +43,24 @@ class StripeController extends AbstractController
                 ],
                 "quantity" => $achat->getQuantity(),
             ];
+            // ajout
             $lineItems[] = $item;
         }
         // dd($lineItems);
+        // route pour validé le payment 
         $successRoute = $this->generateUrl('stripe_valid_payment', [
             "_locale" => $request->getLocale(),
             "commande" => $commande->getId(),
             "stripeSucessKey" => $commande->getStripeSucessKey(),
         ], UrlGeneratorInterface::ABSOLUTE_URL);
         // dd($successRoute);
+        // route en cas d'erreur
         $errorRoute = $this->generateUrl('stripe_error_payment', [
             "_locale" => $request->getLocale(),
             "commande" => $commande->getId(),
         ], UrlGeneratorInterface::ABSOLUTE_URL);
 
+        // rediction vers stripe et redirige vers l'une de route 
         $stripeSession = \Stripe\Checkout\Session::create([
             'line_items' => $lineItems,
             'mode' => 'payment',
@@ -60,6 +69,7 @@ class StripeController extends AbstractController
             'cancel_url' => $errorRoute,
         ]);
         // dd($stripeSession);
+        // mettre a jour commande
         $commande->setPiStripe($stripeSession->payement_intent);
         $entityManagerInterface->flush($commande);
         // dd($entityManagerInterface);
@@ -76,7 +86,7 @@ class StripeController extends AbstractController
             ]);
         }
         $commande->setPaid(true);
-        // on le met dans la session avec un tableau vide //
+        // on le met dans la session
         $session->set('cart', []);
         $achatCriteriav = [
             "commande" => $commande,
@@ -91,6 +101,7 @@ class StripeController extends AbstractController
     #[Route('/stripe/{commande}/cancel', name: 'stripe_error_payment')]
     public function error(Commande $commande, TranslatorInterface $translator ): Response
     {
+        // afficher un message
         $this->addFlash("danger", $translator->trans("src.controller.stripe:There was a problem during the payment process"));
         return $this->redirectToRoute("cart_show");
     }
